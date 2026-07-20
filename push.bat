f145ae93ecae25a7285b7c8468e55ae3bef96979
Pranay Jain <118234723+PranayyJain@users.noreply.github.com>
2026-07-20 13:07:48 +0530

    Delete push.bat

--- push.bat
+++
@@ -1,6 +0,0 @@
-git init
-git checkout -b main
-git remote add origin https://github.com/PranayyJain/yt-reverse.git
-git add .
-git commit -m "Initial release of YT Reverse extension"
-git push -u origin main -f
